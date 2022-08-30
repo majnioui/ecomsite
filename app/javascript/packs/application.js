72ee6579c9b37e1f2f8859561c72ccf1be81ad4e
Khoa Dam <damanhkhoa@gmail.com>
2022-08-30 18:55:47 +0700

    add blogs with action_text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,24 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("bootstrap")
 
 
+import "../stylesheets/application";
+
+
+document.addEventListener("turbolinks:load", function() {
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+  })
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
