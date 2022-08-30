8523593838587926bde893f4e7453bce4b0e74c0
Khoa Dam <damanhkhoa@gmail.com>
2022-08-30 19:06:45 +0700

    add friendly id to blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,7 @@
 class Blog < ApplicationRecord
   has_rich_text :body
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_30_113557) do
+ActiveRecord::Schema.define(version: 2022_08_30_115934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2022_08_30_113557) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
