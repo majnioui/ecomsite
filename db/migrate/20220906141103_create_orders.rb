22486fbc8e6a27cf0b425522e65cac7f4067ada9
Khoa Dam <damanhkhoa@gmail.com>
2022-09-14 14:51:38 +0700

    fix migration syntax

--- db/migrate/20220906141103_create_orders.rb
+++ db/migrate/20220906141103_create_orders.rb
@@ -1,10 +1,11 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
-def change
-  create_table :orders do |t|
-    t.decimal :subtotal # Before discount
-    t.decimal :total  # For any discount you may want to add
-    t.references :user, index: true, null: false, foreign_key: true
-    t.integer :status, default: 0
-    t.timestamps
+  def change
+    create_table :orders do |t|
+      t.decimal :subtotal # Before discount
+      t.decimal :total  # For any discount you may want to add
+      t.references :user, index: true, null: false, foreign_key: true
+      t.integer :status, default: 0
+      t.timestamps
+    end
   end
 end
